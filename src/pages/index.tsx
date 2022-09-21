12c764f6d1a6a35cbc546d008a0058e1f54ed8c7
= <hotalog@hota1024.com>
2022-09-22 06:09:53 +0900

    :tada: (=^・・^=) < ininyalize

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import { NextPage } from 'next'
+import Head from 'next/head'
+
+/**
+ * HomePage component.
+ */
+export const HomePage: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>home - sfc-assist</title>
+      </Head>
+
+      <div>sfc-assist</div>
+    </>
+  )
+}
+
+export default HomePage
