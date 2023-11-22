12d14207acabf32c55869d5cff4c40987f7c8aa8
hota1024 <hotalog@hota1024.com>
2023-11-22 23:08:55 +0900

    💄ブレイクポイントの設定を追加

--- src/stitches.config.ts
+++ src/stitches.config.ts
@@ -58,4 +58,11 @@ export const {
       4: '16px',
     },
   },
+  media: {
+    sm: '(min-width: 640px)',
+    md: '(min-width: 768px)',
+    lg: '(min-width: 1024px)',
+    xl: '(min-width: 1280px)',
+    '2xl': '(min-width: 1536px)',
+  },
 })
