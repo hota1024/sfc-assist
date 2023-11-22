58f6fc5b6097fea53b949f70a648ab8260a96aa8
hota1024 <hotalog@hota1024.com>
2023-11-22 23:09:10 +0900

    🔨link:favicon を追加

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,7 @@ export default class Document extends NextDocument {
       <Html lang="ja">
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
           <link
             rel="preconnect"
             href="https://fonts.gstatic.com"
